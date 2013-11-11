cbc520a0d47fc230032b4a19fd8f8df890de3ddf
Danielle Jenkins <jenkinda@uw.edu>
2013-11-11 15:57:11 -0800

    Fleshed out the documentation for the customer, error, and customerindex classes. Also deleted the customerfactory and factory classes.

--- Lab4/Lab4/error.h
+++ Lab4/Lab4/error.h
@@ -4,16 +4,19 @@
 
 /*
 The error class has a private string datamember that holds an error message.
+
 On construction, error takes in an error message string and assigns it to the
-object's error message datamember. It has an accessor method that returns
-the private error message datamember.
+object's error message datamember. 
+
+It has an accessor method called getErrorMessage that returns the private 
+error message datamember.
 */
 
 class Error {
 private:
     String msg;
 public:
-    Error(String err);
+    Error(String err);  // assigns error message string to datamember string
     ~Error();
     String getErrorMessage() const;
 };
