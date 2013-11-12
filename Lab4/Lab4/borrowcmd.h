edb6eda1a110661861ab0be73d0bcd35b0c108d0
g0tmilk <rioa@uw.edu>
2013-11-11 23:04:42 -0800

    change the return type

--- Lab4/Lab4/borrowcmd.h
+++ Lab4/Lab4/borrowcmd.h
@@ -6,16 +6,31 @@
 #include "command.h"
 
 /*
+The BorrowCMD function will store and verify the borrow 
+command from the users. It will output error messages, if 
+command is not found and it will execute the command if the 
+command exists.
+
+The BorrowCMD is the default constructor for this class 
+The ~BorrowCMD is the default destructor for this class
+The execute command will execute the given command from the IO
+It will return true, if the command is found and return ERROR 
+message if the command is not found.
+The getID is a function that will return the customer ID 
+
 */
 class BorrowCMD : public Command {
 private:
-    CustomerIndex* cIndex;
-    MediaCollection* mColl;
+
+    CustomerIndex* cIndex; //customer Index pointer 
+    MediaCollection* mColl; //media collection pointer 
+
 public:
-    BorrowCMD(mColl, cInd, custID);
-    ~BorrowCMD();
-    execute();
-    getID() const;
+
+    BorrowCMD(mColl, cInd, custID); //default constructor
+    ~BorrowCMD(); //default destructor 
+    bool execute(); //execute command from the IO 
+    int getID() const; //get the customer ID 
 };
 
 #endif
