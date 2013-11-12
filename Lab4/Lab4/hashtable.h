c5e617480816942dd0052a54eae2ff731ba49ffb
g0tmilk <rioa@uw.edu>
2013-11-11 19:08:56 -0800

    add functionality into the hashtable

--- Lab4/Lab4/hashtable.h
+++ Lab4/Lab4/hashtable.h
@@ -1,33 +1,6 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 #include "nodedata.h"
-/*
-The customerindex class that constains all customer objects for each customer 
-who uses the system.   
-
-The class has a private datamember called custHT. It's a hashtable used to
-hold pointers to the customer objects. The key for it is the customerID.
-
-The addTransaction method looks up the customer object in the hashtable 
-using custID as a key, it then calls the customer object's addTransaction
-method using the passed in string as a parameter. If the customer object
-does not exist it creates an error object with an error message string as a 
-parameter, if it does exist, the error object has an empty string.
-
-The customerExists method determines whether the customer object corresponding
-to the custID exists. It attempts to look up the customer object in the 
-hash table using custID as a key, if it exists it returns true, otherwise
-it returns false.
-
-The displayHistory method displays the customer's history by calling the
-customer object's displayHistory method. It looks up the customer object
-in the customer hash table using the custID parameter and if it does not exist
-it returns an Error object with an error message string describing the error,
-otherwise, if the object does exist it returns an Error object with an empty
-string. 
-
-The insertCustomer method takes in a customer pointer and inserts it
-*/
 
 /*
 The hashtable class that maps each search key into a unique location of the hash
