ea99deab36db36b0ac22e0479a40eab227d2704d
Danielle Jenkins <jenkinda@uw.edu>
2013-11-11 18:44:53 -0800

    Updated nodedata, mediadata, and customerdata. Also, redeleted some unused classes

--- Lab4/Lab4/customerdata.h
+++ Lab4/Lab4/customerdata.h
@@ -6,14 +6,34 @@
 
 /*
 The CustomerData class is a child of the NodeData class. It uses polymorphism
+to override various functions.
+
+CustomerData has a private customer pointer.
+
+The 
+
+There is a whole set of comparison operators which are used to compare one
+CustomerData object with another. 
 */
 
 class CustomerData : public NodeData {
 private:
-    Customer* custPtr;
+   Customer* data;                  // points to customer object
 public:
-    CustomerData();
-    ~CustomerData();
+   virtual CustomerData();          // constructor
+   virtual ~CustomerData();         // destructor
+   CustomerData(const Customer*);   // Constructor for customer object
+   virtual string dataType() const; // returns unique identifier        
+   virtual void duplicate();        // LEAVE THIS AN EMPTY METHOD
+   virtual Error getKey();          // returns the customer's ID key
+
+   // comparison operators
+   virtual bool operator==(const CustomerData &) const;
+   virtual bool operator!=(const CustomerData &) const;
+   virtual bool operator<(const CustomerData &) const;
+   virtual bool operator>(const CustomerData &) const;
+   virtual bool operator<=(const CustomerData &) const;
+   virtual bool operator>=(const CustomerData &) const;
 };
 
 #endif
--- Lab4/Lab4/mediadata.h
+++ Lab4/Lab4/mediadata.h
@@ -1,19 +1,27 @@
+#ifndef MEDIADATA_H
+#define MEDIADATA_H
 #include "nodedata.h"
+#include "media.h"
+#include <string>
 
-class MediaData:NodeData {
+class MediaData : public NodeData {
+private:
+   Media* data;          
 public:
-   virtual MediaData();
-   virtual ~MediaData();
-   MediaData(const Media *);
-   string dataType() const;
-   void duplicate();
+   virtual MediaData();      // constructor
+   virtual ~MediaData();     // destructor
+   MediaData(const Media *); // constructor for inserting Media
+   string dataType() const;  // returns unique identifier    
+   void duplicate();  // signals overridden functions to handle its quantity
+   virtual Error getKey(); //
 
+   // comparison operators
    virtual bool operator==(const MediaData &) const;
    virtual bool operator!=(const MediaData &) const;
    virtual bool operator<(const MediaData &) const;
    virtual bool operator>(const MediaData &) const;
    virtual bool operator<=(const MediaData &) const;
    virtual bool operator>=(const MediaData &) const;
-private:
-   Media* data;          
 };
+
+#endif
