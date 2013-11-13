a33d457e30ec62de4d83e728edf540ddd4b1ef0f
g0tmilk <rioa@uw.edu>
2013-11-12 16:53:58 -0800

    commented & modify function

--- Lab4/Lab4/returncmd.h
+++ Lab4/Lab4/returncmd.h
@@ -5,16 +5,28 @@
 #include "command.h"
 
 /*
+This class will return the valid command entered by the user
+If the command is valid, it will execute the command otherwise 
+it will return error 
+
+ReturnCMD is the default constructor for this class, it will create 
+a ReturnCMD object 
+~ReturnCMD is the default constructor 
+execute function checks the valid input, return error for an invalid 
+command and execute the command if it is valid 
 */
+
 class ReturnCMD : public Command {
 private:
-    CustomerIndex* cIndex;
-    MediaCollection* mColl;
-	String mediaInfo;
+
+    CustomerIndex* cIndex; //pointer to the customer index
+    MediaCollection* mColl; //pointer to the media collection 
+    String mediaInfo; //variable to store the variable info 
+
 public:
-    ReturnCMD(mColl, cInd, custID, mediaInfo);
-    ~ReturnCMD();
-    execute();
+    ReturnCMD(mColl, cInd, custID, mediaInfo); //default constructor 
+    ~ReturnCMD(); //default destructor 
+    bool execute(); //validate the cmd input 
 };
 
 #endif
