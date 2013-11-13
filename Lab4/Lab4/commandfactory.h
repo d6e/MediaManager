fed7e0419464db5edd3fcb859a204783e44ad9b9
Danielle Jenkins <jenkinda@uw.edu>
2013-11-12 23:48:35 -0800

    Did some more documentation and updated MediaManager diagram

--- Lab4/Lab4/commandfactory.h
+++ Lab4/Lab4/commandfactory.h
@@ -11,8 +11,11 @@ command based on that command. The rest of the string is passed into the new
 command product.
 
 The CommandFactory constructor that takes a CustomerIndex and a ProductCollection
-is used to  
+is used to pass along the two data structures to it's products.
 
+The create method takes in a string and parses out the command portion of the
+string. It then creates an instance of a child command object based on the 
+command it parsed. Finally, it returns a pointer to that command.
 */
 
 
@@ -21,10 +24,10 @@ private:
     CustomerIndex cIndex;
     ProductCollection mCollect;
 public:
-    CommandFactory();
-	CommandFactory(CustomerIndex, ProductCollection);
-	~CommandFactory();
-    Command* create(String);
+    CommandFactory();   // Constructor
+	CommandFactory(CustomerIndex, ProductCollection);  // constructor
+	~CommandFactory();    // destructor
+    Command* create(string);   // Creates and inits cmd obj based on string
 };
 
 #endif
--- Lab4/Lab4/productformat.h
+++ Lab4/Lab4/productformat.h
@@ -1,7 +1,16 @@
+#ifndef PRODUCTFORMAT_H
+#define PRODUCTFORMAT_H
+
+/*
+
+*/
+
 class ProductFormat {
 public:
 	ProductFormat(string);
 	string getFormat();
 private:
 	string formatName;
-}
+};
+
+#endif
