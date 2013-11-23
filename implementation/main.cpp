247b9e2ddc28df8f907b7fca30afbb9d90995b72
Danielle Jenkins <jenkinda@uw.edu>
2013-11-23 12:50:22 -0800

    Removed customerdata.h and productdata.h because customer and product class can fill in for them

--- implementation/main.cpp
+++ implementation/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include "input.h"
 #include "error.h"
-
+#include "manager.h"
 
 using namespace std;
 
@@ -11,12 +11,13 @@ int main(){
     // TESTING INPUT
     Input I;
     I.read("data.txt");
-    cout << I.get() << endl;
+    // cout << I.get() << endl;
 
     // TESTING ERROR
     Error E("Testing error message");
-    cout << E.getErrorMessage() << endl;
+    // cout << E.getErrorMessage() << endl;
 
+    Manager M();
 
 
     return 0;
--- implementation/manager.cpp
+++ implementation/manager.cpp
@@ -1,1 +1,17 @@
 #include "manager.h"
+
+Manager::Manager(){
+}
+
+
+bool Manager::inputProduct(std::string){
+
+} 
+
+bool Manager::inputCustomer(std::string){
+
+}
+
+bool Manager::inputCmd(std::string){
+
+}
